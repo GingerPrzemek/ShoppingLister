33db8159e237c6f5453b2b454ac930abefd9eede
Przemysław Kamiński <portaltoequestria@gmail.com>
2024-05-24 18:50:49 +0200

    Admin made

--- myapp/polls/models.py
+++ myapp/polls/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    id = models.IntegerField(default=1)
+
+    id = models.BigAutoField(primary_key=True)
     itemName = models.CharField(max_length=200)
     qty = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits= 20, decimal_places=2, default= 0.00)
+    def __str__(self):
+        return self.itemName
