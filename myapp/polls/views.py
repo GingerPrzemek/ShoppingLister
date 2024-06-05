c72ff69bebf9f81e4d50bf561818563701dbb8de
Przemysław Kamiński <portaltoequestria@gmail.com>
2024-06-05 15:08:26 +0200

    Some things displaying on the main page

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
+from .models import Item
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_item_list = Item.objects.order_by("id")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_item_list": latest_item_list,}
+    return render(request, "polls/index.html", context)
+
+def info(request, item_id):
+    return HttpResponse("You are looking at the item number %s" % item_id)
